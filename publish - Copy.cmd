3b308024f6cb79b8b88622141865e89e354f18e6
KevinChang-Sogeti <56926815+KevinChang-Sogeti@users.noreply.github.com>
2019-11-06 10:19:25 -0500

    Revert "Initial development iteration from Kevin Chang."

--- publish - Copy.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotsogetidemo-Web-Deploy.pubxml -p:Password=
-
--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotsogetidemo-Web-Deploy.pubxml -p:Password=MgNyDzxzuPM4uRgFJHnPfGDmwqqtFhPkjpkiS8hdyJ0m7DWezhYLNx3F5Qqs
-
